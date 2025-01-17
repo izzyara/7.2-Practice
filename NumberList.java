849e03ec48d495ce88ffe99e222ed4c9f68fee40
izzyara <izzybzb@gmail.com>
2025-01-17 14:56:52 -0500

    a

---
+++ NumberList.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class NumberList{
+
+    public static void main (String[]args){
+
+        ArrayList<Integer> multiplesOf3 = new ArrayList<Integer>();
+        for (int i = 0; i<100; i++){
+            multiplesOf3.add(i*3);
+        }
+
+        for (int i = 0; i<multiplesOf3.size(); i++){
+            if (multiplesOf3.get(i)%2==0){
+                multiplesOf3.remove(i);
+            }
+        }
+
+        
+        for (int num : multiplesOf3){
+
+            System.out.print(num + ", ");
+            
+        }
+    }
+}
